ec72595c50b394114a1c805c0747b7121305bdd6
gsomapoornan <ganesh.poorna86@gmail.com>
2019-02-14 17:35:40 +0530

    Firstcommit

---
+++ src/main/java/com/visualpath/EcommereceEurekaServer/EcommereceEurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.visualpath.EcommereceEurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EcommereceEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommereceEurekaServerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/visualpath/EcommereceEurekaServer/EcommereceEurekaServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.visualpath.EcommereceEurekaServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EcommereceEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
